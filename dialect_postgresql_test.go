d3aebffbe17cb42ac1c653eb56560f2af7dabd3b
zepatrik <zepatrik@users.noreply.github.com>
2026-03-30 11:46:53 +0200

    fix: use valid self-singed certificate

--- dialect_postgresql_test.go
+++ dialect_postgresql_test.go
@@ -1,9 +1,19 @@
 package pop
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
 	"os"
 	"os/user"
+	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -47,10 +57,69 @@ func Test_PostgreSQL_Connection_String(t *testing.T) {
 	r.Equal("database", cd.Database)
 }
 
+func generateTestCerts(t *testing.T, dir string) (sslcert, sslkey, sslrootcert string) {
+	// Generate CA key pair
+	caKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+
+	caTemplate := &x509.Certificate{
+		SerialNumber:          big.NewInt(1),
+		Subject:               pkix.Name{CommonName: "test-ca"},
+		NotBefore:             time.Now().Add(-time.Minute),
+		NotAfter:              time.Now().Add(time.Hour),
+		IsCA:                  true,
+		KeyUsage:              x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+	caDER, err := x509.CreateCertificate(rand.Reader, caTemplate, caTemplate, &caKey.PublicKey, caKey)
+	require.NoError(t, err)
+
+	// Generate client key pair
+	clientKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+
+	clientTemplate := &x509.Certificate{
+		SerialNumber: big.NewInt(2),
+		Subject:      pkix.Name{CommonName: "test-client"},
+		NotBefore:    time.Now().Add(-time.Minute),
+		NotAfter:     time.Now().Add(time.Hour),
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+	}
+	caCert, err := x509.ParseCertificate(caDER)
+	require.NoError(t, err)
+
+	clientDER, err := x509.CreateCertificate(rand.Reader, clientTemplate, caCert, &clientKey.PublicKey, caKey)
+	require.NoError(t, err)
+
+	// Marshal client private key to DER
+	clientKeyDER, err := x509.MarshalECPrivateKey(clientKey)
+	require.NoError(t, err)
+
+	// Write files
+	sslrootcert = filepath.Join(dir, "root.crt")
+	sslcert = filepath.Join(dir, "client.crt")
+	sslkey = filepath.Join(dir, "client.key")
+
+	writePEM(t, sslrootcert, "CERTIFICATE", caDER)
+	writePEM(t, sslcert, "CERTIFICATE", clientDER)
+	writePEM(t, sslkey, "EC PRIVATE KEY", clientKeyDER)
+
+	return sslcert, sslkey, sslrootcert
+}
+
+func writePEM(t *testing.T, path, pemType string, der []byte) {
+	t.Helper()
+	f, err := os.Create(path)
+	require.NoError(t, err)
+	defer f.Close()
+	require.NoError(t, pem.Encode(f, &pem.Block{Type: pemType, Bytes: der}))
+}
+
 func Test_PostgreSQL_Connection_String_Options(t *testing.T) {
 	r := require.New(t)
 
-	url := "host=host port=1234 dbname=database user=user password=pass# sslmode=disable fallback_application_name=test_app connect_timeout=10 sslcert=/some/location sslkey=/some/other/location sslrootcert=/root/location"
+	sslcert, sslkey, sslrootcert := generateTestCerts(t, t.TempDir())
+	url := fmt.Sprintf("host=host port=1234 dbname=database user=user password=pass# sslmode=disable fallback_application_name=test_app connect_timeout=10 sslcert=%s sslkey=%s sslrootcert=%s", sslcert, sslkey, sslrootcert)
 	cd := &ConnectionDetails{
 		Dialect: "postgres",
 		URL:     url,
